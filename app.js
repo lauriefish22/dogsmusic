0b8494cdb9ce591d239604846bf24f3a7935860b
Laurie Fish <iamlauriefish@gmail.com>
2023-01-24 12:57:16 -0800

    added getBands function

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 /* Imports */
+import { getBands } from './fetch-utils.js';
 
 /* Get DOM Elements */
+const mainEl = document.querySelector('main');
 
 /* State */
+let bandsData = [];
 
 /* Events */
+window.addEventListener('load', () => {
+    const bands = await getBands();
+    bandsData = bands;
+
+    for (let band of bandsData) {
+
+        const bandEl = document.createElement('div');
+        
+    }
+
+});
 
 /* Display Functions */
 
