480aa0fe09da6ed0d08b6be9c3c60961931e9b29
Laurie Fish <iamlauriefish@gmail.com>
2023-01-24 20:27:55 -0800

    tried to fix and didn't work

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 import { getBands } from './fetch.utils.js';
 
 /* Get DOM Elements */
-const mainEl = document.querySelector('main');
+const mainEl = document.querySelector('.main');
 
 /* State */
 let bandsData = [];
@@ -10,25 +10,25 @@ let bandsData = [];
 /* Events */
 window.addEventListener('load', async () => {
     const bands = await getBands();
+    console.log(bands);
 
     bandsData = bands;
-    console.log(bandsData);
-    //displayBands();
+
+    displayBands();
 });
+function displayBands() {
+    for (let band of bandsData) {
+        const bandEl = document.createElement('div');
+        const bandNameEl = document.createElement('p');
+        const leadSingerEl = document.createElement('p');
+        const albumNameEl = document.createElement('p');
 
-for (let band of bandsData) {
-    const bandEl = document.createElement('div');
-    const bandNameEl = document.createElement('p');
-    const leadSingerEl = document.createElement('p');
-    const albumNameEl = document.createElement('p');
+        bandNameEl.textContent = bands.name;
+        leadSingerEl.textContent = bands.lead_id;
+        albumNameEl.textContent = bands.album;
 
-    bandNameEl.textContent = band.name;
-    leadSingerEl.textContent = band.leadSinger;
-    albumNameEl.textContent = band.albumName;
-
-    // bandEl.append(bandNameEl, leadSingerEl, albumNameEl);
-    // mainEl.append(bandEl);
-    // getBands();
-
-    //mainEl.textContent = '';
+        bandEl.append(bandNameEl, leadSingerEl, albumNameEl);
+        mainEl.append(bandEl);
+    }
+    return mainEl;
 }
